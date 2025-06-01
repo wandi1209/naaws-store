5cf1ab5251c097bf8c12163e72759f6ff866a200
wandi1209 <wandider@gmail.com>
2025-06-01 08:35:05 +0700

    chore: create configuration images

---
+++ lib/core/configs/assets/images/app_images.dart
@@ -0,0 +1,17 @@
+class AppImages {
+  static String basePath = "assets/images/";
+  static String categoryIconPath = "${basePath}category-icon/";
+  static String sosmedLogo = "${basePath}social-media/";
+
+  // Logo App
+  final String appLogo = "${basePath}naaws-logo.jpeg";
+
+  // Category Icon
+  final String streamingLogo = "${categoryIconPath}streaming.png";
+  final String musicLogo = "${categoryIconPath}music.png";
+  final String editing = "${categoryIconPath}editing.png";
+  final String toolsLogo = "${categoryIconPath}tools.png";
+
+  // Social Media Logo
+  final String instagramLogo = "${sosmedLogo}instagram-logo.png";
+}
