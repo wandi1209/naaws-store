93d2a9e44f15eaa5cd21b775920a54a62ff76cb2
wandi1209 <wandider@gmail.com>
2025-06-08 07:56:45 +0700

    feat: create responsive for single category widget

--- lib/widgets/single_category.dart
+++ lib/widgets/single_category.dart
@@ -5,9 +5,10 @@ class SingleCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double widthSize = MediaQuery.of(context).size.width;
     return Container(
-      height: 100,
-      width: 200,
+      height: 120,
+      width: widthSize * 0.1,
       color: Colors.grey.shade200,
       child: Text("Category"),
     );
